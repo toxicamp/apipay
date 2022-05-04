ed4af4482d982e5c894e3b56a1fe6f78ad19b370
semenjava <semenjava@gmail.com>
2022-05-05 00:52:38 +0300

    qwe

--- resources/views/profile/userTransact.blade.php
+++ resources/views/profile/userTransact.blade.php
@@ -219,6 +219,7 @@
                             </div>
                         </div>
                     </div>
+                    @endforeach
                 </div>
                 </form>
         </section>
