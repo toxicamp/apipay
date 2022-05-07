032d3fc18b48abcbd350c2af3a5035a650083005
semenjava <semenjava@gmail.com>
2022-05-07 19:36:23 +0300

    fis fiss

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -66,10 +66,8 @@
         <script>
             function getUtc(){
                 var date = new Date();
-                var now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-                    date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
 
-                return new Date(now_utc);
+                return date.toUTCString();
             }
             function blockBy(){
 
