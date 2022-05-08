c2d2e0dfc4169b26f56789d62ddf23b73592c145
semenjava <semenjava@gmail.com>
2022-05-08 12:50:32 +0300

    new job

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -80,7 +80,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'USDT' => $data['USDT'],
-            'UAH' => $data['UAH'],
+            'UAH' => $data['UAH']->where(0),
             'RUB' => $data['RUB'],
             'BTC' => $data['BTC'],
             'global' => $data['global'],
