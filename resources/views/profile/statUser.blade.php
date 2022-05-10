ed6d4ab0c05d28c92e3826d75934e55a3a66efb0
semenjava <semenjava@gmail.com>
2022-05-11 01:57:30 +0300

    new job

--- resources/views/profile/statUser.blade.php
+++ resources/views/profile/statUser.blade.php
@@ -141,7 +141,7 @@
                     <div class="turn-system__wrap">
                         <div class="turn-system__item">
                             <img loading="lazy" src={{asset("img/turn-system__item--icon.png")}} alt="img">
-                        @foreach($trans as $item)  <span>{{$item->sum('total')}} {{$item->currency}} </span> @endforeach
+                            <span> @foreach($trans as $item)  {{$item->sum('total')}} {{$item->currency}} </span> @endforeach
                         </div>
 {{--                        <div class="turn-system__item">--}}
 {{--                            <img loading="lazy" src={{asset("img/turn-system__item--icon2.png")}} alt="img">--}}
