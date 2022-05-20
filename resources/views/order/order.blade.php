ea0e23650ee317de877cbc367c2ae3f8c84dc3bc
semenjava <semenjava@gmail.com>
2022-05-21 00:49:01 +0300

    new job

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -70,7 +70,7 @@
             // var percent = parseInt(strNow.substr(6))/parseInt(strNewData.substr(6))*100;
             // var percent = newDate.getTime()*100/now;
             // var percent = nowNew*100/1200000;
-            var percent = (1200000/nowNew)*100;
+            var percent = (nowNew/1200000)*100;
             console.log(percent);
             var load = document.getElementById('payPolosa');
             load.style.width=percent+'%';
