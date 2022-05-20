cbf587e00ea5ddc69965f9ae20f90fc1f0efec91
semenjava <semenjava@gmail.com>
2022-05-21 00:19:26 +0300

    new job

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -74,7 +74,7 @@ class OrderController extends Controller
         $jsCreateAtt = $createAtt->timestamp*1000.0000009394591;
         $jsCreateAtNot20 = $paymForm->created_at->timestamp*1000.0000009394591;
         $jsNow = $now*1000.0000009394591;
-        $proc = $paymForm->created_at->timestamp*100/$createAt;
+        $proc = 500*100/1000;
         dump($jsCreateAtt, $jsCreateAtNot20, $jsNow, $proc);
 
         if ($now < $createAt && $transaction->status == 'process' && !is_null($transaction->pay_result)){
